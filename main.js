c77b37c87ae8eb24ccad4bae3dfb1825bc194305
Stephanie Mayers <g00286470@stu.raritanval.edu>
2022-04-03 11:43:17 -0400

    Finished

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
 
-Video by Brad Traversy
-JavaScript Crash Course For Beginners
-https://youtu.be/hdI2bqOjy3c?t=4228 
+myForm.addEventListener('submit', onsubmit)
 
-Start at 1:10:29
- */
+function onsubmit(e) {
+    e.preventDefault()
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all fields'
+
+        setTimeout(() => msg.remove(), 3000)
+    } else {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(`
+        ${nameInput.value} : ${emailInput.value}`))
+
+        userList.appendChild(li)
+
+        nameInput.value = ''
+        emailInput.value = ''
+    }
+}
